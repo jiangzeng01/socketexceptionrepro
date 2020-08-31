4dc6dc91d37dcfdf89be5497972fa54af1266c41
jiangzeng01 <49379637+jiangzeng01@users.noreply.github.com>
2020-08-31 17:35:05 +0800

    Init
    init

---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+	public partial class Form1 : Form
+	{
+		private static TcpListener listener;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		private void button1_Click(object sender, EventArgs e)
+		{
+			listener = new TcpListener(IPAddress.Any, 4441);
+			listener.Start(6);
+			listener.BeginAcceptSocket(Listener_AcceptSocketCallback, null);
+			label1.Text = "started listening";
+		}
+		private static void Listener_AcceptSocketCallback(IAsyncResult ar)
+		{
+			try
+			{
+				Socket client = listener.EndAcceptSocket(ar);
+			}
+			catch (ObjectDisposedException ex)
+			{
+				MessageBox.Show("ObjectDisposedException on 3.1 and 5 prev");
+				//catch on 3.1 and 5 prev
+			}
+			catch (Exception ex)
+			{
+				//catch on .net5 rc
+				MessageBox.Show($"Catch on .net5 RC {ex.Message}");
+			  	listener.BeginAcceptSocket(Listener_AcceptSocketCallback, null);
+			}
+		}
+		private void button2_Click(object sender, EventArgs e)
+		{
+			listener.Stop();
+			label1.Text = "stopped";
+		}
+	}
+}
